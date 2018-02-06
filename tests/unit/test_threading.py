36f14bf6d0db9f21b1518db17ba0c4b4ae958a33
seaders <seaders@gmail.com>
2018-02-06 11:27:44 +0000

    Simplify runtime error check.

--- tests/unit/test_threading.py
+++ tests/unit/test_threading.py
@@ -6,21 +6,23 @@ import time
 
 class BaseThreadingTest(unittest.TestCase):
     def test_snap(self):
-        self._caches = {'1.{}'.format(i): None for i in range(10000)}
+        self._caches = {'1.{}'.format(i): None for i in range(100)}
         self.t_cont = True
 
         def _change_dict():
+            i = len(self._caches)
             while self.t_cont:
-                self._caches['1.2'] = 1
-                self._caches.pop('1.2')
+                i += 1
+                self._caches['1.{}'.format(i)] = 1
 
         t = threading.Thread(target=_change_dict)
         t.start()
 
-        for _ in range(10):
-            for _ in self._caches.values():
-                pass
-
-        self.t_cont = False
+        try:
+            for _ in range(100):
+                for _ in self._caches.values():
+                    pass
+        finally:
+            self.t_cont = False
 
 #
