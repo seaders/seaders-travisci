0863a7c3f374010a2531e698d31b01e4a22df668
seaders <seaders@gmail.com>
2018-02-06 12:05:19 +0000

    Bring back delay and multiple runs :(

--- tests/unit/test_threading.py
+++ tests/unit/test_threading.py
@@ -9,10 +9,16 @@ class BaseThreadingTest(unittest.TestCase):
         caches = {'1.{}'.format(i): None for i in range(10000)}
         self._caches = dict(caches.items())
         self.t_cont = True
+        self.t_delay = True
 
         def _change_dict():
             i = len(self._caches)
             while self.t_cont:
+                while self.t_delay:
+                    self._caches = dict(caches.items())
+                    i = len(self._caches)
+                    time.sleep(0.01)
+
                 i += 1
                 self._caches['1.{}'.format(i)] = 1
 
@@ -20,12 +26,17 @@ class BaseThreadingTest(unittest.TestCase):
         t.start()
 
         try:
-            for _ in self._caches.values():
-                pass
+            for _ in range(5):
+                self.t_delay = False
+                for _ in self._caches.values():
+                    pass
+                self.t_delay = True
 
         finally:
             self.t_cont = False
+            self.t_delay = False
 
         self.t_cont = False
+        self.t_delay = False
 
 #
