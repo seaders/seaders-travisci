1aab95a149591c68318c5f76597d991757800499
seaders <seaders@gmail.com>
2018-02-06 12:00:07 +0000

    Test 1 run (should insta-break python3, no break & no delay python2).

--- tests/unit/test_threading.py
+++ tests/unit/test_threading.py
@@ -9,15 +9,10 @@ class BaseThreadingTest(unittest.TestCase):
         caches = {'1.{}'.format(i): None for i in range(10000)}
         self._caches = dict(caches.items())
         self.t_cont = True
-        self.t_delay = True
 
         def _change_dict():
             i = len(self._caches)
             while self.t_cont:
-                while self.t_delay:
-                    self._caches = caches
-                    time.sleep(0.001)
-
                 i += 1
                 self._caches['1.{}'.format(i)] = 1
                 time.sleep(0.001)
@@ -26,13 +21,12 @@ class BaseThreadingTest(unittest.TestCase):
         t.start()
 
         try:
-            for _ in range(100):
-                self.t_delay = False
-                for _ in self._caches.values():
-                    pass
-                self.t_delay = True
+            for _ in self._caches.values():
+                pass
 
         finally:
             self.t_cont = False
+            
+        self.t_cont = False
 
 #
